00aa282563af90258927d118d71bf4d9c398c293
mtchllng <mtchllng@gmail.com>
2011-12-16 20:32:56 -0800

    please work

--- facebook-drupal/sites/all/modules/fb/fb_tab.js
+++ facebook-drupal/sites/all/modules/fb/fb_tab.js
@@ -7,12 +7,22 @@
 /**
  * Enable canvas page specific javascript on this page.
  */
-Drupal.behaviors.fb_tab = function(context) {
-  // Resize if body class includes fb_canvas-resizable.
-  $('body.fb_tab-resizable:not(.fb_tab-processed)').each(function () {
-    $(this).addClass('fb_tab-processed');
-    jQuery(document).bind('fb_init', FB_Tab.setAutoResize);
-  });
+Drupal.behaviors.fb_tab = {
+  attach: function(context, settings) {
+    // Resize if body class includes fb_canvas-resizable.
+    jQuery('body.fb_tab-resizable:not(.fb_tab-processed)').each(function () {
+      jQuery(this).addClass('fb_tab-processed');
+      if (typeof(FB) == 'undefined') {
+        // FB not yet initialized.
+        jQuery(document).bind('fb_init', FB_Tab.setAutoResize);
+      }
+      else {
+        // FB already initialized.
+        FB_Tab.setAutoResize();
+      }
+      jQuery(document).bind('fb_init', FB_Tab.setAutoResize);
+    });
+  }
 };
 
 FB_Tab = function(){};
